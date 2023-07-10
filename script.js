618e252072cb4abb1b3aafe302b9ed240ffebfe8
ionutcatalins <stanciuionutcatalin95@gmail.com>
2023-07-10 14:58:20 +0300

    Add all my files

---
+++ script.js
@@ -0,0 +1,17 @@
+const words = new Array(1000);
+let numberWords = 0
+
+function buttonSave() {
+    words[numberWords] = document.getElementById("in").value;
+    ++numberWords
+    document.getElementById('in').value = '';
+}
+function searchWords() {
+    let search = document.getElementById("in").value;  
+    document.getElementById('in').value = '';  
+    if(words.includes(search)) {
+        document.getElementById("myResult").innerHTML = "Cuvantul se afla in dictionar"
+    } else {
+        document.getElementById("myResult").innerHTML = "Cuvantul nu se afla in dictionar"
+    }
+}
